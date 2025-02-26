ed5df4c058cf05594165b8546060f95da05f8fd3
Alfred Sam <alfredsam2006@gmail.com>
2025-02-26 23:28:17 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -134,8 +134,8 @@ def send_emails():
                     # Certificate Processing
                     if template_file and data:
                         template = Image.open(template_file)
-                        for row in data:
-                            if row["email"].strip() == recipient:
+                        row = next((r for r in data if r["email"].strip() == recipient), None)
+                            if row:
                                 template_copy = template.copy()
                                 cert_buffer, _ = generate_certificate(template_copy, row, font_file, positions, font_sizes, file_type)
 
